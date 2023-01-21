bb806b6ee9bd6820449b422e83300fd75a682995
NIZE0000 <anupong.niceone@gmail.com>
2023-01-21 21:28:15 +0700

    add google_fonts and edit theme

--- app_clone/lib/main.dart
+++ app_clone/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_clone/screen/auth/login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'KCKQueue',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+
+        // You can add the color from the seprate class here as well to maintain it well.
+        primaryColor: Colors.lightBlue[800],
+
+        // Define the default font family.
+        fontFamily: 'Prompt',
+
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
       ),
       debugShowCheckedModeBanner: false,
       home: const Login(),
