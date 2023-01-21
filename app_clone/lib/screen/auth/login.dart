a48739cd5516eeba0a189e90f38413f9eaef4266
NIZE0000 <anupong.niceone@gmail.com>
2023-01-21 21:15:01 +0700

    add login file

---
+++ app_clone/lib/screen/auth/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
