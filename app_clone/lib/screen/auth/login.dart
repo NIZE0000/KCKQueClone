eb3f14717761e84b84fe6de81556f52045e80a81
NIZE0000 <anupong.niceone@gmail.com>
2023-01-22 01:19:01 +0700

    add logo image

--- app_clone/lib/screen/auth/login.dart
+++ app_clone/lib/screen/auth/login.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -10,18 +12,27 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
         appBar: AppBar(title: const Text("LOGIN")),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 100,
-              width: 100,
-              child:
-                  ElevatedButton(child: const Text("Login"), onPressed: () {}),
-            )
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Image.asset("assets/images/logoonly.png"),
+                ],
+              ),
+              SizedBox(
+                width: screenWidth * 0.8,
+                height: 50,
+                child: ElevatedButton(
+                    child: const Text("Login"), onPressed: () {}),
+              )
+            ],
+          ),
         ));
   }
 }
